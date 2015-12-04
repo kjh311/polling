ae2b242e85acd14b3e54ca355cbba9dacbdc1164
Kevin Huelsmann <kjh311@gmail.com>
2015-12-04 09:19:04 -0800

    first commit

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,36 @@
+// Controller for the poll list
+function PollListCtrl($scope, Poll) {
+  $scope.polls = Poll.query();
+}
+
+// Controller for an individual poll
+function PollItemCtrl($scope, $routeParams, socket, Poll) {
+  $scope.poll = Poll.get({pollId: $routeParams.pollId});
+
+  socket.on('myvote', function(data) {
+    console.dir(data);
+    if(data._id === $routeParams.pollId) {
+      $scope.poll = data;
+    }
+  });
+
+  socket.on('vote', function(data) {
+    console.dir(data);
+    if(data._id === $routeParams.pollId) {
+      $scope.poll.choices = data.choices;
+      $scope.poll.totalVotes = data.totalVotes;
+    }
+  });
+
+  $scope.vote = function() {
+    var pollId = $scope.poll._id,
+        choiceId = $scope.poll.userVote;
+
+    if(choiceId) {
+      var voteObj = { poll_id: pollId, choice: choiceId };
+      socket.emit('send:vote', voteObj);
+    } else {
+      alert('You must select an option to vote for');
+    }
+  };
+}
